c1a5a6d894e1377b7f19971fb620ec327dbb408b
Cloud User <ec2-user@ip-172-31-29-190.ec2.internal>
2023-10-30 20:21:06 +0000

    Adicionando os as versões dos códigos paralelos e sequencial do problema do trapezio

---
+++ Lab06-Parte2/trapezioSeq.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double f(double x){
+  return exp(x);
+}
+
+double line_area(double a,double b,int n){
+  double x;
+  double h = (b-a)/n;
+  double approx = (f(a) + f(b))/2.0;
+  
+  for(int i = 1; i <= n-1; i++){
+    x = a + i*h;
+    approx += f(x);
+  }
+  approx = h*approx;
+  
+  return approx;
+}
+
+int main(){
+  double a,b,result;
+  int n;
+  n = 10000000; //Numero de trapezios
+  a = 0.0;
+  b = 1.0;
+
+  result = line_area(a,b,n);
+  printf("%.14e\n",result);
+
+}
